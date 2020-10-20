660f5267b5c132a8617b46079b20699419a8f17a
Roberto Rossa <rcrossa@hotmail.com>
2020-10-20 20:58:56 -0300

    Agregar archivos de proyecto.

---
+++ BlazorApp2/Data/ListadoT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BlazorApp2.Data
+{
+    public class ListadoT
+    {
+        public int idTarea { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Vencimiento { get; set; }
+        public int Estimacion { get; set; }        
+        public string Estado { get; set; }
+        public ListadoT() { }
+
+        public ListadoT(int idTarea, string titulo, DateTime Vencimiento, int Estimacion,
+            string Estado)
+        {
+            this.idTarea = idTarea;
+            Titulo = titulo;
+            this.Vencimiento = Vencimiento;
+            this.Estimacion = Estimacion;
+            this.Estado = Estado;
+        }
+    }
+    
+}
